773e05dbacef84468cebc7596f4786c82bcdf69a
sherylmarianasarria5-pixel <sherylmarianasarria5@gmail.com>
2026-03-30 14:51:11 -0500

    fix puerto render

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 6001;
+const PORT = process.env.PORT || 6001;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
